451bdf3cb154fb1c792aff15c0224099b98b8bb7
vanhooktristan <vanhooktristan@gmail.com>
2018-06-30 11:07:33 -0500

    commit

--- server.js
+++ server.js
@@ -1,50 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Require our dependecies
-var express = require("express");
-var mongoose = require("mongoose");
-var dotenv = require('dotenv').config()
-var bluebird = require("bluebird");
-var bodyParser = require("body-parser");
-var routes = require("./routes/routes");
-var apiController = require('./controllers/apiController')
-
-// Set up a default port, configure mongoose, configure our middleware
-var PORT = process.env.PORT || 4000;
-mongoose.Promise = bluebird;
-var app = express();
+// Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/build"));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-var db = process.env.MONGODB_URI || "mongodb://localhost/NYT-react";
+// Add routes, both API and view
+app.use(routes);
 
-// Connect mongoose to our database
-mongoose.connect(db, function(error) {
-  if (error) {
-    console.error(error);
-  }
-  else {
-    console.log("mongoose connection is successful");
-  }
-});
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/nytarticles"
+);
 
-// enable CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next();
-});
-
-app.use("/", routes);
-
-app.post("/api/articles", apiController.create);
-
-app.get("/api/articles", apiController.index);
-
-app.delete("/api/articles/:id", apiController.destroy);
-
-// Start the server
-app.listen(PORT, function() {
-  console.log("Now listening on port %s! Visit localhost:%s in your browser.", PORT, PORT);
-});
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
